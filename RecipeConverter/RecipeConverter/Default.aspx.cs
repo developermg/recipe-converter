507a99a7c3147337ba14a5ae64cbb3587c25f6c2
MG <meiracgoldberg@gmail.com>
2017-05-04 09:42:43 -0400

    Commented code-behind

--- RecipeConverter/RecipeConverter/Default.aspx.cs
+++ RecipeConverter/RecipeConverter/Default.aspx.cs
@@ -10,21 +10,39 @@ namespace RecipeConverterApp
 {
     public partial class _Default : Page
     {
-
+        /// <summary>
+        /// The SubmitButton_Click method triggers submission
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void SubmitButton_Click(object sender, EventArgs e)
         {
             Submit();
         }
 
+        /// <summary>
+        /// The ResetButton_Click triggers reloading the page
+        /// Because reseting from the server will send page back, 
+        /// makes sense to reload the page to clear rather than clear all the fields and results boxes
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         protected void ResetButton_Click(object sender, EventArgs e)
         {
-            ResetWholePage();
+            ReloadPage();
         }
 
-        private void ResetWholePage()
+        /// <summary>
+        /// The ReloadPage method reloads the page
+        /// </summary>
+        private void ReloadPage()
         {
             Response.Redirect(Request.Url.PathAndQuery, true);
         }
+
+        /// <summary>
+        /// The Submit method submits the recipe  and displays the results if the serving sizes are valid
+        /// </summary>
         protected void Submit()
         {
             int desiredServings, originalServings;
@@ -36,12 +54,14 @@ namespace RecipeConverterApp
             {
                 try
                 {
-                    ResizedVersionLabel.Text = "<i>Servings: " + desiredServings + "</i><br/>" + ConvertRecipe(IngredientsText.Text, desiredServings, originalServings); //.Replace("\r\n", "<br/>");
+                    ResizedVersionLabel.Text = "<i>Servings: " + desiredServings + "</i><br/>" + ConvertRecipe(IngredientsText.Text, desiredServings, originalServings);
 
                 }
+                //if any Exception is thrown when converteing the recipe, catch it and display an error message.
                 catch(Exception ex)
                 {
-                    DisplayError("Error converting recipe.");
+                    //display the exception message in small so user can submit the error
+                    DisplayError("Error converting recipe.<small>Exception: " + ex.Message + "</small>");
                 }
             }
             else
@@ -50,6 +70,13 @@ namespace RecipeConverterApp
             }
         }
 
+        /// <summary>
+        /// The ConvertRecipe method converts a recipe using the RecipeConverter
+        /// </summary>
+        /// <param name="ingredients">Text of ingredients</param>
+        /// <param name="desiredServings">Desired number of servings</param>
+        /// <param name="originalServings">Original number of servings</param>
+        /// <returns>New text</returns>
         private string ConvertRecipe(string ingredients, int desiredServings, int originalServings)
         {
             RecipeConverter converter = new RecipeConverter(ingredients, new NonNegativeFraction(desiredServings, originalServings));
