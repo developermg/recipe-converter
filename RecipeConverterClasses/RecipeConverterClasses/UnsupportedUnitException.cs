3f20e9ddf4199f52a6dc044bc7d83e58455978fa
MG <meiracgoldberg@gmail.com>
2017-05-04 12:29:14 -0400

    Make custom exception extend ApplicationException

--- RecipeConverterClasses/RecipeConverterClasses/UnsupportedUnitException.cs
+++ RecipeConverterClasses/RecipeConverterClasses/UnsupportedUnitException.cs
@@ -6,11 +6,12 @@ using System.Threading.Tasks;
 
 namespace RecipeConverterClasses
 {
-[Serializable] public class UnsupportedUnitException : Exception 
+[Serializable] public class UnsupportedUnitException : ApplicationException
     {
         public UnsupportedUnitException() { }
         public UnsupportedUnitException(string message) : base(message) { }
         public UnsupportedUnitException(string message, Exception inner) : base(message, inner) { }
+
         protected UnsupportedUnitException(
           System.Runtime.Serialization.SerializationInfo info,
           System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
